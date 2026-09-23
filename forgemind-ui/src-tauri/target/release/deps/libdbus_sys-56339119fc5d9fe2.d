a18b7b8b55d5add616ea6aa244a7dec47cf78903
灵感引擎工坊 <bingfeng41@qq.com>
2026-09-24 04:38:00 +0800

    chore: remove tauri build artifact

--- forgemind-ui/src-tauri/target/release/deps/libdbus_sys-56339119fc5d9fe2.d
+++
@@ -1,7 +0,0 @@
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/quantmind/quantmind-ui/src-tauri/target/release/deps/libdbus_sys-56339119fc5d9fe2.d: /workspace/.home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libdbus-sys-0.2.7/src/lib.rs
-
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/quantmind/quantmind-ui/src-tauri/target/release/deps/liblibdbus_sys-56339119fc5d9fe2.rlib: /workspace/.home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libdbus-sys-0.2.7/src/lib.rs
-
-/run/csi/mount-root/nas/eab0d61a99b6696edb3d2aff87b585e8/quantmind/quantmind-ui/src-tauri/target/release/deps/liblibdbus_sys-56339119fc5d9fe2.rmeta: /workspace/.home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libdbus-sys-0.2.7/src/lib.rs
-
-/workspace/.home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libdbus-sys-0.2.7/src/lib.rs:
